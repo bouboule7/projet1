406ae700f10ab0e7197af3a7ad6acdff5babd6fe
Jeanny Fidelica <93310908+bouboule7@users.noreply.github.com>
2022-01-20 13:21:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./asset/css/style.css">
     <title>Saisie d'un prêt</title>
 </head>
 <body>
