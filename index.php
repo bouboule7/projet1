516995b4ad4a992354dd5aebd56a7f0acec6c70e
Jeanny Fidelica <93310908+bouboule7@users.noreply.github.com>
2022-01-20 13:10:20 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,42 +3,104 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-<link rel="stylesheet" href="bootstrap.css"">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/form.css">
+    <title>Saisie d'un prêt</title>
 </head>
 <body>
-    <h1>Hello Bootstrap 4</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Succes</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-
-</br>
-
-    <form>
-        <div class="form-group">
-            <label for="txtMail">eMail</label>
-            <input type="email" class="form-control" id="txtMail" placeholder="eMail">
+    <main class="d-flex justify-content-center"><div class="wrapper">
+    <div class="py-2 mb-3">
+        <h2 class="text-light text-center">Formulaire de saisie d'un prêt</h2>
+        <div class="underline"></div>
+    </div>
+    <form class="form container-fluid rounded p-2 p-md-3">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="client">Client</label>
+                <select name="client" id="client" class="form-control reg-opacity">
+                    <option value="boa">BOA</option>
+                    <option value="avitech">Avitech</option>
+                    <option value="accesBanque">Accès Banque</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="resp">Responsable</label>
+                <select name="choixresp" class="form-control reg-opacity" id="resp">
+                    <option value="agent1">Agent 1</option>
+                    <option value="agent2">Agent 2</option>
+                </select>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="txtPassword">password</label>
-            <input type="password" class="form-control" id="txtPassword" placeholder="Password">
+        <div class="row">
+            <div class="col-md-6">
+                <label class="sr-only" for="inlineFormInputGroup">Montant prêt</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                    <div class="input-group-text">Ar</div>
+                    </div>
+                    <input type="number" name="pret" class="form-control reg-opacity" id="inlineFormInputGroup" min="0" placeholder="Montant prêt">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <label for="percentage">Pourcentage</label>
+                <input type="number" name="pour" id="percentage" max="100" class="form-control reg-opacity">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="txtFile">File Selection</label>
-            <input type="file" id="txtFile">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="loanDate">Date du prêt</label>
+                <input type="date" class="form-control reg-opacity" id="loanDate">
+            </div>
+            <div class="col-md-6">
+                <label for="reimbursementDate">Date de remboursement</label>
+                <input type="date" class="form-control reg-opacity" id="reimbursementDate">
+            </div>
         </div>
-        <div class="input-group">
-            <span class="input-group-addon" id="basic-addon1">@</span>
-            <input type="text" class="form-control" placeholder="User" aria-describedby="basic-addon1" aria-describedby="basic-addon2">
-            <span class="input-group-addon" id="basic-addon2">22</span>
+        <div class="row">
+            <div class="col-md-6">
+                <label for="paymentMode">Mode de paiement du capital</label>
+                <select name="paymentMode" id="paymentMode" class="form-control reg-opacity">
+                    <option>Virement bancaire</option>
+                    <optgroup label="Mobile Money">
+                        <option>MVola</option>
+                        <option>Airtel Money</option>
+                        <option>Orange Money</option>
+                    </optgroup>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="interestPaymentMode">Mode de paiement des intérêts</label>
+                <select name="interestPaymentMode" id="interestPaymentMode" class="form-control reg-opacity">
+                    <option>Virement bancaire</option>
+                    <optgroup label="Mobile Money">
+                        <option>MVola</option>
+                        <option>Airtel Money</option>
+                        <option>Orange Money</option>
+                    </optgroup>
+                </select>
+            </div>
         </div>
+        <div class="row">
+            <div class="col-md-6">
+                <label for="reimbursementMode">Mode de remboursement</label>
+                <select name="reimbursementMode" id="reimbursementMode" class="form-control reg-opacity">
+                    <option>Mensuel</option>
+                    <option>Hebdomadaire</option>
+                    <option>Journalier</option>
+                </select>
+            </div>
+            <div class="col-md-12">
+                <label for="comment">Commentaire</label>
+                <textarea name="comment" id="comment" rows="5" class="form-control reg-opacity" placeholder="Ajouter un commentaire..."></textarea>
+            </div>
+        </div>
+        <div class="d-flex justify-content-between mt-3"><a href="#" class="note">Ajouter une note...</a><input type="submit" class="btn btn-secondary" value="Enregistrer"></div>
     </form>
-                                                             <a href="enregistrement.html">lien</a>                                                        
+    <div class="mt-3"><a href="./assets/html/list.html" class="btn btn-primary">Accéder à la liste des prêts</a></div>
+    </div></main>
+    
+    <!-- <script src="bootstrap/js/jquery-3.6.0.min.js"></script> -->
+    <script src="assets/css/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
